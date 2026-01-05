cfc64ba0492f7bf776af6469aa2aec4f13c87452
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2026-01-05 11:56:45 +0630

    Constructor

---
+++ Constructor/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Student st1 = new Student("Kyaw",23,4.6);
+		Student st2 = new Student("Kyaw",23,2.6);
+		
+		System.out.println(st1.name);
+		st1.Study();
+
+	}
+
+}
---
+++ Constructor/src/Student.java
@@ -0,0 +1,19 @@
+
+public class Student {
+	
+	String name;
+	int age;
+	double gpa;
+	boolean isEnrolled;
+
+
+Student(String name,int age, double gpa){
+	this.name = name;
+	this.age = age;
+	this.gpa = gpa;
+}
+void Study() {
+	System.out.println("is Studying");
+}
+
+}
